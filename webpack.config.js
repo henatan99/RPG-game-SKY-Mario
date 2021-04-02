bd6b6c6b3d48425f66b5bee47f6be0d0019fc53f
Henok <henatan99@gmail.com>
2021-04-02 20:02:38 +0300

    installs dependencies: babel, jest, phasor, file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'none', 
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        // clean: true,
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: '.dist',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+    
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            // type: 'asset/resource',
+            use: [
+              'file-loader',
+            ],
+          },
+    
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+        ],
+      },
 };
