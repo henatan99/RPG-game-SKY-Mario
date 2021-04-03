6777177e76c5b2750f8cbd8d04012775a6762b25
Henok <henatan99@gmail.com>
2021-04-03 13:46:15 +0300

    setup simple game template with conifg and GameScene files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Phaser from 'phaser';
+import config from './Config/config';
+import GameScene from './Scenes/GameScene';
+ 
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
+ 
+window.game = new Game();
