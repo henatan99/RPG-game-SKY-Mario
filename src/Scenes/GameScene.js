c77f5da956a0669589435fce992229983daa04eb
Henok <henatan99@gmail.com>
2021-04-05 19:48:09 +0300

    loads assets in GameScene function and defines platforms variable using physics method, and adds physics support in config.js file

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,4 +1,9 @@
 import 'phaser';
+import sky from '../assets/WorldAssets/sky.png';
+import platform from '../assets/WorldAssets/platform.png';
+import star from '../assets/WorldAssets/star.png';
+import dude from '../assets/WorldAssets/dude.png';
+import bomb from '../assets/WorldAssets/bomb.png';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -7,10 +12,25 @@ export default class GameScene extends Phaser.Scene {
 
   preload () {
     // load images
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('sky', sky);
+    this.load.image('ground', platform);
+    this.load.image('star', star);
+    this.load.image('bomb', bomb);
+    this.load.spritesheet('dude', 
+        dude,
+        { frameWidth: 32, frameHeight: 48 }
+    );
   }
 
   create () {
-    this.add.image(400, 300, 'logo');
+    this.add.image(400, 300, 'sky');
+
+    var platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
   }
 };
