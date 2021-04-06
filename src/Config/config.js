0da137f37efffdf0eee141920d557cd989eeb38a
Henok <henatan99@gmail.com>
2021-04-06 13:18:36 +0300

    adds player and cursor objects to the GameScene

--- src/Config/config.js
+++ src/Config/config.js
@@ -8,7 +8,7 @@ export default {
   physics: {
     default: 'arcade',
     arcade: {
-        gravity: { y: 300 },
+        gravity: { y: 5000 },
         debug: false
     }
   },
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,7 +7,7 @@ import bomb from '../assets/WorldAssets/bomb.png';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
-    super('Game');
+    super('Game');  
   }
 
   preload () {
@@ -32,5 +32,63 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(this.player, platforms);       
+  }
+
+  update () {
+    this.player.body.setVelocity(0);
+
+    var cursors = this.input.keyboard.createCursorKeys(); 
+
+    if (cursors.left.isDown)
+    {
+        this.player.setVelocityX(-160);
+
+        this.player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        this.player.setVelocityX(160);
+
+        this.player.anims.play('right', true);
+    }
+    else
+    {
+        this.player.setVelocityX(0);
+
+        this.player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && this.player.body.touching.down)
+    {
+        this.player.setVelocityY(-6000);
+    }
   }
 };
