448154b49eb6f993ffefda1e9f1d8d7522e7ce08
Henok <henatan99@gmail.com>
2021-04-14 15:24:22 +0300

    fixes bugs in setData and CreditScene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -30,7 +30,7 @@ export default class CreditsScene extends Phaser.Scene {
       duration: 3000,
       delay: 1000,
       onComplete() {
-        this.destroy();
+        this.destroy;
       },
     });
 
@@ -41,7 +41,7 @@ export default class CreditsScene extends Phaser.Scene {
       duration: 8000,
       delay: 1000,
       onComplete: function () {
-        this.madeByTween.destroy();
+        this.madeByTween.destroy;
         this.scene.start('Title');
       }.bind(this),
     });
